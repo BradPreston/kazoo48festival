d8c6707059eb0ac5c2af81cbc29df84706493e37
bradpreston <bap5393@gmail.com>
2022-03-27 16:01:20 -0400

    fixed vercel deploy issue

--- pages/success.tsx
+++ pages/success.tsx
@@ -7,8 +7,8 @@ const Success = () => {
       <h1>Good luck in the show!</h1>
       <p>
         You'll get an email with your reciept shortly. If you don't recieve an
-        email, please contact us at{' '}
-        <a href="mailto:kazoo48film@gmail.com">kazoo48film@gmail.com</a>
+        email, please contact us at
+        <a href="mailto:kazoo48film@gmail.com"> kazoo48film@gmail.com</a>
       </p>
       <Link href="/">
         <a className={styles.home}>Go Home</a>
