ef132186dfb11d2d1561b42e2a7096b0774ee9e6
bradpreston <bap5393@gmail.com>
2023-01-19 17:25:14 -0500

    added public stripe price

--- pages/api/checkout_sessions.ts
+++ pages/api/checkout_sessions.ts
@@ -13,7 +13,7 @@ export default async function handler(
         line_items: [
           {
             // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-            price: process.env.STRIPE_PRICE,
+            price: process.env.NEXT_PUBLIC_STRIPE_PRICE,
             quantity: 1
           }
         ],
